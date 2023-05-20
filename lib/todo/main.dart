e8faa9e66d8a4173f2fc36dd0160390ef95a9882
asma <asma.khalil@enit-utm.tn>
2023-05-20 22:51:28 +0200

    committed

--- lib/todo/main.dart
+++ lib/todo/main.dart
@@ -5,9 +5,18 @@ import '../identificcation/login.dart';
 import 'package:get/get.dart';
 import 'savetasks.dart';
 import '../Provider/provider.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import '../todo/todo.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+Future<void> backroundHandler(RemoteMessage message) async {
+  print(" This is message from background");
+  print(message.notification!.title);
+  print(message.notification!.body);
+}
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backroundHandler);
   Get.put(TaskRepository());
   Get.put(TaskProvider());
 
@@ -21,7 +30,8 @@ class MyApp extends StatelessWidget {
    return ChangeNotifierProvider(
        create: (context)=>TaskProvider(),
      child: MaterialApp(
-       home: Login(),
+
+       home:  Login(),
      ),
 
    );
